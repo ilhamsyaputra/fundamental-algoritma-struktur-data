4857b653d0f160e485f14a52d21021252d675832
Ilham <ilhamsyaputra25@gmail.com>
2022-05-04 01:17:51 +0700

    add node deletion - Binary Search Tree

--- 06-binary-search-tree/binary_search_tree.py
+++ 06-binary-search-tree/binary_search_tree.py
@@ -6,7 +6,7 @@ class BinarySearchTree:
 
     def insert(self, value, node = None):
         node = node or self.root
-
+ 
         if node is None:
             self.root = Node(value)
             node = self.root
@@ -20,3 +20,41 @@ class BinarySearchTree:
                 node.right = Node(value)
             else:
                 self.insert(value, node.right)
+
+    def leftmost_node(self, node):
+        if node.left is None:
+            return node
+        return self.leftmost_node(node.left)
+
+    def delete(self, value, node = None, parent_node = None, mark = None):
+        node = node or self.root
+
+        if node is None:
+            return None
+        
+        if value == node.value:
+            deleted_node = node
+
+            if node == self.root:
+                if node.left is None and node.right is None:
+                    self.root = None
+                else:
+                    self.root = self.leftmost_node(node.right)
+            else:
+                if mark == 'left':
+                    if node.left is not None and node.right is None:
+                        parent_node.left = node.left
+                    else:
+                        parent_node.left = node.right
+                elif mark == 'right':
+                    if node.right is not None and node.left is None:
+                        parent_node.right = node.right
+                    else:
+                        parent_node.right = node.left
+
+        elif value < node.value:
+            deleted_node = self.delete(value, node.left, node, 'left')
+        elif value > node.value:
+            deleted_node = self.delete(value, node.right, node, 'right')
+            
+        return deleted_node
--- 06-binary-search-tree/binary_search_tree_test.py
+++ 06-binary-search-tree/binary_search_tree_test.py
@@ -72,4 +72,161 @@ class TestBST(unittest.TestCase):
         BST.insert(5)
         BST.insert(7)
         self.assertEqual(BST.root.right.left.value, 5)
+
+    def test_deletion_1(self):
+        """
+        should return None when deleting any value from an empty tree
+        """
+        BST = BinarySearchTree()
+        self.assertEqual(BST.delete(1), None)
+
+    def test_deletion_2(self):
+        """
+        should return root node when deleting root node
+        """
+        BST = BinarySearchTree()
+        BST.insert(1)
+        deleted_node = BST.delete(1)
+        self.assertEqual(deleted_node.value, 1)
         
+    def test_deletion_3(self):
+        """
+        should actually delete the root node
+        """
+        BST = BinarySearchTree()
+        BST.insert(1)
+        BST.delete(1)
+        self.assertEqual(BST.root, None)
+
+    def test_deletion_4(self):
+        """
+        should return left child node when deleting child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(2)
+        BST.insert(1)
+        deleted_node = BST.delete(1)
+        self.assertEqual(deleted_node.value, 1)
+
+    def test_deletion_5(self):
+        """
+        should actually delete left child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(2)
+        BST.insert(1)
+        BST.delete(1)
+        self.assertEqual(BST.root.left, None)
+
+    def test_deletion_6(self):
+        """
+        should return right child node when deleting child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(1)
+        BST.insert(2)
+        deleted_node = BST.delete(2)
+        self.assertEqual(deleted_node.value, 2)
+
+    def test_deletion_7(self):
+        """
+        should actually delete right child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(1)
+        BST.insert(2)
+        BST.delete(2)
+        self.assertEqual(BST.root.right, None)
+
+    def test_deletion_8(self):
+        """
+        should ensure left grandchild node become the new child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(3)
+        BST.insert(2)
+        BST.insert(1)
+        BST.delete(2)
+        self.assertEqual(BST.root.left.value, 1)
+
+    def test_deletion_9(self):
+        """
+        should ensure right grandchild node become the new child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(1)
+        BST.insert(2)
+        BST.insert(3)
+        BST.delete(2)
+        self.assertEqual(BST.root.right.value, 3)
+
+    def test_deletion_10(self):
+        """
+        should ensure right grandchild node become new left child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(3)
+        BST.insert(1)
+        BST.insert(2)
+        BST.delete(1)
+        self.assertEqual(BST.root.left.value, 2)
+
+    def test_deletion_11(self):
+        """
+        should ensure left grandchild node become new right child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(1)
+        BST.insert(3)
+        BST.insert(2)
+        BST.delete(3)
+        self.assertEqual(BST.root.right.value, 2)
+
+    def test_deletion_12(self):
+        """
+        should ensure left-most node of the right subtree become the new left child node
+        """
+        BST = BinarySearchTree()
+        BST.insert(4)
+        BST.insert(2)
+        BST.insert(5)
+        BST.insert(1)
+        BST.insert(3)
+        BST.delete(2)
+        self.assertEqual(BST.root.left.value, 3)
+    
+    def test_deletion_13(self):
+        """
+        should ensure left most node of the right subtree become the new root node
+        """
+        BST = BinarySearchTree()
+        BST.insert(2)
+        BST.insert(1)
+        BST.insert(4)
+        BST.insert(3)
+        BST.insert(5)
+        BST.delete(2)
+        self.assertEqual(BST.root.value, 3)
+
+    def test_deletion_14(self):
+        """
+        should ensure left most node of the right subtree become the new parent node
+        """
+        BST = BinarySearchTree()
+        BST.insert(8)
+        BST.insert(4)
+        BST.insert(12)
+        BST.insert(2)
+        BST.insert(6)
+        BST.insert(10)
+        BST.insert(14)
+        BST.insert(1)
+        BST.insert(3)
+        BST.insert(5)
+        BST.insert(7)
+        BST.insert(9)
+        BST.insert(11)
+        BST.insert(13)
+        BST.insert(15)
+        BST.delete(8)
+        self.assertEqual(BST.root.value, 9)
